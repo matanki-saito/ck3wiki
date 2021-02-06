18b5b13ff0d4fc9d263ff5c6aeafa6128b181a61
matanki.saito <matanki.saito@gmail.com>
2021-02-06 20:01:31 +0900

    監視テストの追加

--- LocalSettings.php
+++ LocalSettings.php
@@ -119,7 +119,7 @@ $wgDiff3 = "/usr/bin/diff3";
 
 # The following permissions were set based on your choice in the installer
 $wgGroupPermissions['*']['createaccount'] = true;
-$wgGroupPermissions['*']['edit'] = false;
+$wgGroupPermissions['*']['edit'] = true;
 
 ## Default skin: you can change the default skin. Use the internal symbolic
 ## names, ie 'vector', 'monobook':
